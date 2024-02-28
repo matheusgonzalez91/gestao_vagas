d191d7c40d6ed18f5b988f4538dbdefe54a8528b
matheusgonzalez91 <gonzalezmatheus01@gmail.com>
2024-02-28 18:33:45 -0300

    Inserindo validação para os campos

--- src/main/java/br/com/matheusdev/gestaovagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/matheusdev/gestaovagas/modules/candidate/CandidateEntity.java
@@ -1,15 +1,25 @@
 package br.com.matheusdev.gestaovagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
 @Data
 public class CandidateEntity {
+
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "\\S+", message = "O campo [USERNAME] não pode conter espaços.")
     private String username;
+
+    @Email(message = "O campo [EMAIL] deve conter um e-mail válido.")
     private String email;
+
+    @Length(min = 10, max = 100, message = "A senha deve conter de [10] a [100] caracteres.")
     private String password;
     private String description;
     private String curriculum;
