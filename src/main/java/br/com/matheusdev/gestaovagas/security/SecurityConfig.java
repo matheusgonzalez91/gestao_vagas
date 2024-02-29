7466e6d5ed7ea5578891bc41e7b093e5d135a6c3
matheusgonzalez91 <gonzalezmatheus01@gmail.com>
2024-02-28 22:16:16 -0300

    Configurando rotas públicas

--- src/main/java/br/com/matheusdev/gestaovagas/security/SecurityConfig.java
+++ src/main/java/br/com/matheusdev/gestaovagas/security/SecurityConfig.java
@@ -10,7 +10,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate/").permitAll()
+                            .requestMatchers("/company/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+        ;
         return http.build();
     }
 
